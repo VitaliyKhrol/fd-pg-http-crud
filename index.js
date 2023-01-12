38b9baa33ae2c55ce35b3da57fe4d1c5f8aa6740
VitaliyKhrol <VitaliyX@gmail.com>
2023-01-12 09:39:51 +0200

    cat crud done

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const app = require('./app');
+
+const PORT = 6000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(`App started on port ${PORT}`)
+});
